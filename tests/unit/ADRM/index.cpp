cfbf42cfea8b0c24fc01595fbcfabac13b113ffc
Joryms <joryms@gmail.com>
2021-05-05 20:34:54 -0600

    fix: readded ZPY tests

--- tests/unit/ADRM/index.cpp
+++ tests/unit/ADRM/index.cpp
@@ -5,5 +5,6 @@
 #include "tests/IMM.cpp" // Immediate Address Mode
 #include "tests/ZP.cpp" // Zero Page Address Mode
 #include "tests/ZPX.cpp" // Zero Page X Address Mode
+#include "tests/ZPY.cpp" // Zero Page X Address Mode
 #include "tests/AB.cpp" // Absolute Address Mode
 #include "tests/ABX.cpp" // Absolute X Address Mode
