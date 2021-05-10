f684ef1b1f156b3472d1ecf4787b97107cb61d2c
Joryms <joryms@gmail.com>
2021-05-10 14:49:53 -0600

    test: added tests for implied addressing mode

--- tests/unit/ADRM/index.cpp
+++ tests/unit/ADRM/index.cpp
@@ -2,12 +2,13 @@
 
 // Unit Test Files
 
-#include "tests/IMM.cpp" // Immediate Address Mode
-#include "tests/ZP.cpp" // Zero Page Address Mode
-#include "tests/ZPX.cpp" // Zero Page X Address Mode
-#include "tests/ZPY.cpp" // Zero Page X Address Mode
 #include "tests/AB.cpp" // Absolute Address Mode
 #include "tests/ABX.cpp" // Absolute X Address Mode
 #include "tests/ABY.cpp" // Absolute Y Address Mode
+#include "tests/IMM.cpp" // Immediate Address Mode
+#include "tests/IMP.cpp" // Immediate Address Mode
 #include "tests/IDX.cpp" // Indexed Indirect Address Mode
 #include "tests/IDY.cpp" // Indirect Indexed Address Mode
+#include "tests/ZP.cpp" // Zero Page Address Mode
+#include "tests/ZPX.cpp" // Zero Page X Address Mode
+#include "tests/ZPY.cpp" // Zero Page X Address Mode
