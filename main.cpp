59dcb488f68eb53f85abe3cadd9b8b9f982229f4
Joryms <joryms@gmail.com>
2021-05-15 00:30:02 -0600

    chore: removed old test files

--- main.cpp
+++ main.cpp
@@ -1,9 +1,25 @@
-#include "src/hardware.cpp"
+#include "src/cpu.cpp"
+#include <stdio.h>
 
 int main() {
-    // Mem mem;
-    // CPU cpu;
+    Mem mem = Mem();
+    CPU cpu = CPU(&mem);
 
+    for (int i = 0; i < 0xFF; i++) {
+        mem[0x0100] = (Byte) 0xA0;
+        mem[0x5795] = (Byte) 0x00;
+        cpu.PC = 0x0100;
+        cpu.A = (Byte) i;
+
+        cpu.Run();
+
+        printf("A is %X; Mem is %X\n", cpu.A, mem[0x5795]);
+    }
+
+    // for (int i = 0; i < 0xff; i++) {
+    //     Byte value = cpu.Decode((Byte) i);
+    //     printf("Received value of %X for OP Code %X\n", value, (Byte) i);
+    // }
     return 0;
 }
 
