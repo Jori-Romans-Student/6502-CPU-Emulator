122bed82b2a1d0a5fc862ca44d657f89be0360e0
Joryms <joryms@gmail.com>
2021-05-14 23:20:27 -0600

    chore: reorganized file structure

--- tests.cpp
+++ tests.cpp
@@ -1,4 +1,33 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch2/catch.hpp"
 
-#include "tests/unit/index.cpp" // Unit Tests
+// Common Methods
+
+#include "tests/unit/methods/read.cpp"
+#include "tests/unit/methods/write.cpp"
+#include "tests/unit/methods/fetch.cpp"
+
+// Addressing
+
+#include "tests/unit/addressing/AB.cpp"
+#include "tests/unit/addressing/ABX.cpp"
+#include "tests/unit/addressing/ABY.cpp"
+#include "tests/unit/addressing/AC.cpp"
+#include "tests/unit/addressing/ID.cpp"
+#include "tests/unit/addressing/IDX.cpp"
+#include "tests/unit/addressing/IDY.cpp"
+#include "tests/unit/addressing/IMM.cpp"
+#include "tests/unit/addressing/REL.cpp"
+#include "tests/unit/addressing/ZP.cpp"
+#include "tests/unit/addressing/ZPX.cpp"
+#include "tests/unit/addressing/ZPY.cpp"
+
+// Instructions
+
+#include "tests/unit/instructions/LDA.cpp"
+#include "tests/unit/instructions/LDX.cpp"
+#include "tests/unit/instructions/LDY.cpp"
+#include "tests/unit/instructions/STA.cpp"
+#include "tests/unit/instructions/STX.cpp"
+#include "tests/unit/instructions/STY.cpp"
+
