28e5afed637d0bae3405653186531461cf148cef
Joryms <jori.romans@telus.com>
2021-05-29 10:17:48 -0600

    test: added full run tests for branch codes

---
+++ tests/unit/cpu/runs/branch/BEQ.cpp
@@ -0,0 +1,54 @@
+#include "catch2/catch.hpp"
+#include "../../constants.hpp"
+
+TEST_CASE("BEQ Run Tests") {
+
+    // Initialize Hardware
+
+    Mem mem = Mem();
+    CPU cpu = CPU(&mem);
+
+    // Global Vars
+
+    Word PC = random<Word>();
+    Byte value = random<Byte>();
+    Bit Z = random<Bit>();
+
+    // Local Vars
+
+    Byte OPCode;
+    Word address;
+    Byte displacement;
+
+    // Configure CPU
+
+    cpu.Z = Z;
+    cpu.PC = PC;
+
+    SECTION("REL addressing mode test config") {
+
+        // Config
+
+        OPCode = 0xF0;
+        address = PC + 1;
+
+    };
+
+    // Set memory locations based on section parameters
+
+    mem[PC] = OPCode;
+    mem[address] = value;
+
+    // Run based on section parameters
+
+    cpu.Run();
+
+    // Displacement
+
+    isSet(Z) ? displacement = value : displacement = 0;
+
+    // Assertions
+
+    REQUIRE(cpu.PC == (PC + 2 + (Signed) displacement));
+
+}
---
+++ tests/unit/cpu/runs/branch/BMI.cpp
@@ -0,0 +1,54 @@
+#include "catch2/catch.hpp"
+#include "../../constants.hpp"
+
+TEST_CASE("BMI Run Tests") {
+
+    // Initialize Hardware
+
+    Mem mem = Mem();
+    CPU cpu = CPU(&mem);
+
+    // Global Vars
+
+    Word PC = random<Word>();
+    Byte value = random<Byte>();
+    Bit N = random<Bit>();
+
+    // Local Vars
+
+    Byte OPCode;
+    Word address;
+    Byte displacement;
+
+    // Configure CPU
+
+    cpu.N = N;
+    cpu.PC = PC;
+
+    SECTION("REL addressing mode test config") {
+
+        // Config
+
+        OPCode = 0x30;
+        address = PC + 1;
+
+    };
+
+    // Set memory locations based on section parameters
+
+    mem[PC] = OPCode;
+    mem[address] = value;
+
+    // Run based on section parameters
+
+    cpu.Run();
+
+    // Displacement
+
+    isSet(N) ? displacement = value : displacement = 0;
+
+    // Assertions
+
+    REQUIRE(cpu.PC == (PC + 2 + (Signed) displacement));
+
+}
